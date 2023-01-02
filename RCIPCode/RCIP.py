aa45d5331afea67d73955b452866fe0a48015130
Hirish Chandrasekaran <hirish@ucsb.edu>
2023-01-02 09:32:32 -0800

    File structure change

--- RCIPCode/RCIP.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import cmath as cm
-from scipy.linalg import block_diag
-from scipy.sparse.linalg import gmres
-
--- RCIPCode/UnitTestsRCIP.py
+++
@@ -1,1 +0,0 @@
-from RCIP import *
