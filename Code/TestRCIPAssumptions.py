7aa44ecbe834f7b7e18d68fc07ac164650224b94
Hirish Chandrasekaran <hirish@ucsb.edu>
2023-03-29 22:54:14 -0500

    everything changes at same rate up until solve, solve seems to be issue

--- Code/TestRCIPAssumptions.py
+++ Code/TestRCIPAssumptions.py
@@ -6,6 +6,25 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 import unittest
 
 class TestNaiveMethod(unittest.TestCase):
+
+    def test_convergence_of_KcircR_old_new(self):
+        npan = 10
+
+        old_arr = []
+        new_arr = []
+        x = []
+
+        for i in range(10, 30, 1):
+            old_arr.append(np.linalg.norm(old_rcip_problem_KcircR(npan, i)-old_rcip_problem_KcircR(npan, i-1))/np.linalg.norm(old_rcip_problem_KcircR(npan, i)))
+            new_arr.append(np.linalg.norm(teardrop_rcip_improved_KcircR(npan, i)-teardrop_rcip_improved_KcircR(npan, i-1))/np.linalg.norm(teardrop_rcip_improved_KcircR(npan, i)))
+            x.append(i)
+
+        plt.loglog(x, old_arr, 'o',label='old rcip code')
+        plt.loglog(x, new_arr, 'o',label='new rcip code')
+        plt.title("KcircR old kernel vs new kernel")
+        plt.legend()
+        plt.show()
+
     def test_convergence_of_LHS(self):
         npan = 10
 
@@ -14,7 +33,7 @@ class TestNaiveMethod(unittest.TestCase):
         x = []
 
         for i in range(10, 30, 1):
-            old_arr.append(np.linalg.norm(old_rcip_problem_density(npan, i)[0]-old_rcip_problem_density(npan, i-1)[0])/np.linalg.norm(old_rcip_problem_density(npan, i)[0]))
+            old_arr.append(np.linalg.norm(old_rcip_problem_LHS_RHS(npan, i)[0]-old_rcip_problem_LHS_RHS(npan, i-1)[0])/np.linalg.norm(old_rcip_problem_LHS_RHS(npan, i)[0]))
             new_arr.append(np.linalg.norm(teardrop_rcip_improved_LHS_RHS(npan, i)[0]-teardrop_rcip_improved_LHS_RHS(npan, i-1)[0])/np.linalg.norm(teardrop_rcip_improved_LHS_RHS(npan, i)[0]))
             x.append(i)
 
