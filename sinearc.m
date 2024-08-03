a23befcd5e7c0e6f7f553c2f5a3313ad0bffcc58
ShawnL00 <63332547+ShawnL00@users.noreply.github.com>
2024-08-03 16:31:56 +0800

    Add files via upload
    Test

---
+++ sinearc.m
@@ -0,0 +1,12 @@
+function [r,d,d2] = sinearc(t,amp,frq)
+xs = t;
+ys = amp*sin(frq*t);
+xp = ones(size(t));
+yp = amp*frq*cos(frq*t);
+xpp = zeros(size(t));
+ypp = -frq*frq*amp*sin(t);
+
+r = [(xs(:)).'; (ys(:)).'];
+d = [(xp(:)).'; (yp(:)).'];
+d2 = [(xpp(:)).'; (ypp(:)).'];
+end
