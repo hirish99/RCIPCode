ad5d0d979d29b00e8ddc1064e83d7dd900e4b6d6
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-08-08 17:15:09 -0700

    Add QBX Kernel

---
+++ QBX_kernel/green.m
@@ -0,0 +1,11 @@
+function val = green(radius, order,  src, targ, varargin)
+    %QBX.GREEN evaluate the Dirac green's function (after Fourier Transform)
+    %for the given sources and targets
+    
+    % Gamma matrix
+    val = 0;
+    
+    end
+    
+    
+    
---
+++ QBX_kernel/kern.m
@@ -0,0 +1,51 @@
+function submat = kern(radius, order, srcinfo, targinfo, varargin)
+    %QBX.KERN   Evaluate the QBX kernel Laplace 2D 
+
+    % WHAT IS varagin?
+    
+    % The Greens function of the Dirac operator (after Fourier transform in z)
+    % is given by the sum of three components:
+    
+    % 1. gamma_1 .* (\nabla_{x} G_{zk}(x,y) \dot [1;0])
+    % 2. gamma_2 .* (\nabla_{x} G_{zk}(x,y) \dot [1;0])
+    % 3. [m.*gamma0 + w.*gamma5 + E.*I_4 + i*\xi.*gamma3 ] .* G_{zk}(x,y)
+    
+    % The sum of the above kernel should be scaled by (2*pi)^(-1/2) due to the
+    % Fourier transform
+    
+    
+    % Input:
+    %   r - expansion radius
+    %   srcinfo - description of sources in ptinfo struct format, i.e.
+    %                ptinfo.r - positions (2,:) array
+    %                ptinfo.d - first derivative in underlying
+    %                     parameterization (2,:)
+    %                ptinfo.d2 - second derivative in underlying
+    %                     parameterization (2,:)
+    %   targinfo - description of targets in ptinfo struct format,
+    %                if info not relevant (d/d2) it doesn't need to
+    %                be provided. sprime requires tangent info in
+    %                targinfo.d
+    %
+    % Output:
+    %   submat - the evaluation of the kernel for the
+    %            provided sources and targets. the number of
+    %            rows equals the number of targets and the
+    %            number of columns equals the number of sources  
+    %
+    % see also QBX.GREEN
+    
+    
+    
+    src = srcinfo.r;
+    targ = targinfo.r;
+    
+    submat = dirac2d.green(radius, order, src, targ, varargin);
+    
+    end
+    
+    
+    
+    
+    
+    
